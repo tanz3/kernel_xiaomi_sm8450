908f451b1f9611006ee72a39eb7a5821dc499064
tanz3 <3222839916@qq.com>
2022-07-05 18:37:56 +0800

    net: wireless: Fix multiple definition after building inline
    Signed-off-by: tanz3 <3222839916@qq.com>

--- drivers/net/wireless/cnss2/qmi.h
+++ drivers/net/wireless/cnss2/qmi.h
@@ -283,12 +283,12 @@ int cnss_wlfw_qdss_data_send_sync(struct cnss_plat_data *plat_priv, char *file_n
 
 static inline void cnss_dms_deinit(struct cnss_plat_data *plat_priv) {}
 
-int wlfw_qdss_trace_start(struct cnss_plat_data *plat_priv)
+int wlfw_qdss_trace_send_start(struct cnss_plat_data *plat_priv)
 {
 	return 0;
 }
 
-int wlfw_qdss_trace_stop(struct cnss_plat_data *plat_priv, unsigned long long option)
+int wlfw_qdss_trace_send_stop(struct cnss_plat_data *plat_priv, unsigned long long option)
 {
 	return 0;
 }
